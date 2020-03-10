c719692ebdc67edab333a3d84bd9c78005f08cd3
Adriano <desenvolvimento@intersea.com.br>
2020-03-10 09:15:47 -0300

    Inicio

---
+++ webpack.mix.js
@@ -0,0 +1,24 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.styles([
+                'resources/views/css/form.css',     //---css origem
+                'resources/views/css/table.css',    //---css origem
+            ], 'public/css/style.css')              //---css destino para atender as páginas html
+
+    .scripts([
+                'resources/views/js/form.js'        //---js origem
+            ], 'public/js/script.js')                //---js destino para atender as páginas html
+
+    .version();                                     //---Para não ter problemas com cash de css... essa função cria o arquivo mix-manifest.json
+

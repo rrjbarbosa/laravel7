70aa830ea557ea0d41ce625fcafc071c0739b17b
Adriano <desenvolvimento@intersea.com.br>
2020-03-10 17:28:17 -0300

    bacup

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,18 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.sass('resources/sass/app.scss', 'public/css/bootstrap.css')
+    .styles([
+        'resources/views/css/form.css',     //---css origem
+        'resources/views/css/table.css',    //---css origem
+    ], 'public/css/style.css')              //---css destino para atender as páginas html
+
+    .scripts('node_modules/jquery/dist/jquery.js', 'public/js/jquery.js')
+
+    .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'public/js/bootstrap.js')
+
+    .scripts([
+        'resources/views/js/form.js'        //---js origem
+    ], 'public/js/script.js')               //---js destino para atender as páginas html
+
+    .version();                             //---Para não ter problemas com cash de css... essa função cria o arquivo mix-manifest.json
